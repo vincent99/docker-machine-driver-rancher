4ecadb3caf5bf11eab6fd71202f1ae9cb73e888d
Vincent Fiduccia <vincent@rancher.com>
2016-07-29 01:17:46 -0700

    It's alive-ish

---
+++ Makefile
@@ -0,0 +1,59 @@
+.PHONY : build dist dist-clean fmt release tag-release deps vet test
+
+NAME=docker-machine-driver-rancher
+VERSION := $(shell cat VERSION)
+
+ifneq ($(CIRCLE_BUILD_NUM),)
+	BUILD:=$(VERSION)-$(CIRCLE_BUILD_NUM)
+else
+	BUILD:=$(VERSION)
+endif
+
+LDFLAGS:=-X main.Version=$(VERSION)
+
+all: build
+
+build:
+	mkdir -p build
+	go build -a -ldflags "$(LDFLAGS)" -o build/$(NAME)-$(BUILD) ./bin
+
+dist-clean:
+	rm -rf dist
+	rm -rf release
+
+dist: dist-clean
+	mkdir -p release
+	mkdir -p dist
+	mkdir -p dist/linux/amd64 && GOOS=linux GOARCH=amd64 go build -a -ldflags "$(LDFLAGS)" -o dist/linux/amd64/$(NAME) ./bin
+	mkdir -p dist/linux/armhf && GOOS=linux GOARCH=arm GOARM=6 go build -a -ldflags "$(LDFLAGS)" -o dist/linux/armhf/$(NAME) ./bin
+	mkdir -p dist/darwin/amd64 && GOOS=darwin GOARCH=amd64 go build -a -ldflags "$(LDFLAGS)" -o dist/darwin/amd64/$(NAME) ./bin
+	mkdir -p dist/windows/amd64 && CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -a -ldflags "$(LDFLAGS)" -o dist/windows/amd64/$(NAME).exe ./bin
+	tar -cvzf release/$(NAME)-$(VERSION)-linux-amd64.tar.gz -C dist/linux/amd64 $(NAME)
+	cd $(shell pwd)/release && md5sum $(NAME)-$(VERSION)-linux-amd64.tar.gz > $(NAME)-$(VERSION)-linux-amd64.tar.gz.md5
+	tar -cvzf release/$(NAME)-$(VERSION)-linux-armhf.tar.gz -C dist/linux/armhf $(NAME)
+	cd $(shell pwd)/release && md5sum $(NAME)-$(VERSION)-linux-armhf.tar.gz > $(NAME)-$(VERSION)-linux-armhf.tar.gz.md5
+	tar -cvzf release/$(NAME)-$(VERSION)-darwin-amd64.tar.gz -C dist/darwin/amd64 $(NAME)
+	cd $(shell pwd)/release && md5sum $(NAME)-$(VERSION)-darwin-amd64.tar.gz > $(NAME)-$(VERSION)-darwin-amd64.tar.gz.md5
+	tar -cvzf release/$(NAME)-$(VERSION)-windows-amd64.tar.gz -C dist/windows/amd64 $(NAME).exe
+	cd $(shell pwd)/release && md5sum $(NAME)-$(VERSION)-windows-amd64.tar.gz > $(NAME)-$(VERSION)-windows-amd64.tar.gz.md5
+
+release: dist
+	ghr -u vincent99 -r docker-machine-driver-rancher --replace $(VERSION) release/
+
+tag-release:
+	git tag -f `cat VERSION`
+	git push -f origin master --tags
+
+deps:
+	go get -u github.com/tcnksm/ghr
+
+vet:
+	@if [ -n "$(shell gofmt -l .)" ]; then \
+		echo 1>&2 'The following files need to be formatted:'; \
+		gofmt -l .; \
+		exit 1; \
+	fi
+	godep go vet .
+
+test:
+	godep go test -race ./...
---
+++ rancher.go
@@ -0,0 +1,359 @@
+package rancher
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"time"
+
+	"github.com/docker/machine/libmachine/drivers"
+	"github.com/docker/machine/libmachine/log"
+	"github.com/docker/machine/libmachine/mcnflag"
+	"github.com/docker/machine/libmachine/ssh"
+	"github.com/docker/machine/libmachine/state"
+	rancher "github.com/rancher/go-rancher/client"
+)
+
+type Driver struct {
+	*drivers.BaseDriver
+	Url       string
+	AccessKey string
+	SecretKey string
+
+	OsImage  string
+	MemoryMb int
+	Vcpu     int
+	//	RootDiskGb				int
+	//	DataDiskGb				int
+
+	client *rancher.RancherClient
+
+	MachineId string
+	IPAddress string
+}
+
+const (
+	defaultOsImage  = "rancher/vm-ubuntu"
+	defaultMemoryMb = 1024
+	defaultVcpu     = 2
+	//	defaultRootDiskGb= 20
+	//	defaultDataDiskGb= 0
+	clientMaxRetries = 5
+)
+
+// GetCreateFlags registers the flags this driver adds to
+// "docker hosts create"
+func (d *Driver) GetCreateFlags() []mcnflag.Flag {
+	return []mcnflag.Flag{
+		mcnflag.StringFlag{
+			EnvVar: "RANCHER_URL",
+			Name:   "rancher-url",
+			Usage:  "Rancher Environment URL",
+		},
+		mcnflag.StringFlag{
+			EnvVar: "RANCHER_ACCESS_KEY",
+			Name:   "rancher-access-key",
+			Usage:  "Rancher Access Key",
+		},
+		mcnflag.StringFlag{
+			EnvVar: "RANCHER_SECRET_KEY",
+			Name:   "rancher-secret-key",
+			Usage:  "Rancher Secret Key",
+		},
+		mcnflag.StringFlag{
+			EnvVar: "RANCHER_OS_IMAGE",
+			Name:   "rancher-os-image",
+			Usage:  "Rancher OS Image.  Default: " + defaultOsImage,
+			Value:  defaultOsImage,
+		},
+		mcnflag.IntFlag{
+			EnvVar: "RANCHER_MEMORY_MB",
+			Name:   "rancher-memory-mb",
+			Usage:  "Memory in MiB. Default: " + strconv.Itoa(defaultMemoryMb),
+			Value:  defaultMemoryMb,
+		},
+		mcnflag.IntFlag{
+			EnvVar: "RANCHER_VCPU",
+			Name:   "rancher-vcpu",
+			Usage:  "Number of virtual CPUs. Default: " + strconv.Itoa(defaultVcpu),
+			Value:  defaultMemoryMb,
+		},
+		/*
+			mcnflag.IntFlag{
+				EnvVar: "RANCHER_ROOT_DISK_GB",
+				Name:   "rancher-root-disk-gb",
+				Usage:  "Root disk size in GiB. Default: " + strconv.Itoa(defaultRootDiskGb),
+				Value:  defaultRootDiskGb,
+			}
+			mcnflag.IntFlag{
+				EnvVar: "RANCHER_DATA_DISK_GB",
+				Name:   "rancher-data-disk-gb",
+				Usage:  "Root disk size in GiB. Default: " + strconv.Itoa(defaultDataDiskGb),
+				Value:  defaultDataDiskGb,
+			}
+		*/
+	}
+}
+
+func NewDriver(hostName, storePath string) *Driver {
+	d := &Driver{
+		OsImage:  defaultOsImage,
+		MemoryMb: defaultMemoryMb,
+		Vcpu:     defaultVcpu,
+		//RootDiskGb:			defaultRootDiskGb,
+		//DataDiskGb:			defaultDataDiskGb,
+
+		BaseDriver: &drivers.BaseDriver{
+			MachineName: hostName,
+			StorePath:   storePath,
+		},
+	}
+	return d
+}
+
+func (d *Driver) GetSSHHostname() (string, error) {
+	return d.GetIP()
+}
+
+// DriverName returns the name of the driver
+func (d *Driver) DriverName() string {
+	return "rancher"
+}
+
+func (d *Driver) SetConfigFromFlags(flags drivers.DriverOptions) error {
+	d.Url = flags.String("rancher-url")
+	d.AccessKey = flags.String("rancher-access-key")
+	d.SecretKey = flags.String("rancher-secret-key")
+	d.OsImage = flags.String("rancher-os-image")
+	d.MemoryMb = flags.Int("rancher-memory-mb")
+	d.Vcpu = flags.Int("rancher-vcpu")
+	//d.RootDiskGb = flags.Int("rancher-root-disk-gb")
+	//d.DataDiskGb = flags.Int("rancher-data-disk-gb")
+
+	if d.Url == "" {
+		return fmt.Errorf("Rancher driver requires the --rancher-url option")
+	}
+
+	if d.AccessKey == "" {
+		return fmt.Errorf("Rancher driver requires the --rancher-access-key option")
+	}
+
+	if d.SecretKey == "" {
+		return fmt.Errorf("Rancher driver requires the --rancher-secret-key option")
+	}
+
+	return nil
+}
+
+func (d *Driver) PreCreateCheck() error {
+	return nil
+}
+
+func (d *Driver) Create() error {
+	log.Debug("Generating SSH key...")
+
+	key, err := d.createSSHKey()
+	if err != nil {
+		return err
+	}
+
+	userdata := `		
+#cloud-config
+
+ssh_authorized_keys:
+	- "` + key + `"`
+
+	log.Info("Creating Rancher VM...")
+
+	if userdata != "" {
+		log.Debugf("Using the following Cloud-init User Data:")
+		log.Debugf("%s", userdata)
+	}
+
+	client := d.getClient()
+
+	machine, err := client.Create(&rancher.VirtualMachine{
+		Name:      d.MachineName,
+		ImageUuid: "docker:" + d.OsImage,
+		MemoryMb:  int64(d.MemoryMb),
+		Vcpu:      int64(d.Vcpu),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	d.MachineId = machine.Id
+
+	log.Infof("Waiting for VM %s to become available...", d.MachineId)
+	for {
+		machine, err = d.getMachine()
+		if err != nil {
+			return err
+		}
+		d.IPAddress = machine.PrimaryIpAddress
+
+		if d.IPAddress != "" && machine.State == "active" {
+			break
+		}
+
+		log.Debug("VM not yet available")
+		time.Sleep(2 * time.Second)
+	}
+
+	log.Infof("Created Rancher VM ID: %s, Public IP: %s",
+		d.MachineId,
+		d.IPAddress,
+	)
+
+	return nil
+}
+
+func (d *Driver) createSSHKey() (string, error) {
+	if err := ssh.GenerateSSHKey(d.GetSSHKeyPath()); err != nil {
+		return "", err
+	}
+
+	publicKey, err := ioutil.ReadFile(d.publicSSHKeyPath())
+	if err != nil {
+		return "", err
+	}
+
+	return string(publicKey), nil
+}
+
+func (d *Driver) GetURL() (string, error) {
+	s, err := d.GetState()
+	if err != nil {
+		return "", err
+	}
+
+	if s != state.Running {
+		return "", drivers.ErrHostIsNotRunning
+	}
+
+	ip, err := d.GetIP()
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("tcp://%s:2376", ip), nil
+}
+
+func (d *Driver) GetIP() (string, error) {
+	if d.IPAddress == "" || d.IPAddress == "0" {
+		return "", fmt.Errorf("IP address is not set")
+	}
+	return d.IPAddress, nil
+}
+
+func (d *Driver) getMachine() (*rancher.VirtualMachine, error) {
+	return d.getClient().ById(d.MachineId)
+}
+
+func (d *Driver) GetState() (state.State, error) {
+	machine, err := d.getMachine()
+
+	if err != nil {
+		return state.Error, err
+	}
+
+	switch machine.State {
+	case "creating":
+	case "migrating":
+	case "requested":
+	case "restarting":
+	case "restoring":
+	case "starting":
+		return state.Starting, nil
+
+	case "error":
+	case "erroring":
+	case "purged":
+	case "purging":
+	case "removed":
+	case "removing":
+		return state.Error, nil
+
+	case "running":
+	case "updating-running":
+		return state.Running, nil
+
+	case "stopped":
+	case "updating-stopped":
+		return state.Stopped, nil
+
+	case "stopping":
+		return state.Stopping, nil
+	}
+
+	return state.None, nil
+}
+
+func (d *Driver) Start() error {
+	machine, err := d.getMachine()
+	if err != nil {
+		return err
+	}
+
+	log.Debugf("Starting %s", d.MachineName)
+	_, err = d.getClient().ActionStart(machine)
+	return err
+}
+
+func (d *Driver) Stop() error {
+	machine, err := d.getMachine()
+	if err != nil {
+		return err
+	}
+
+	log.Debugf("Stopping %s", d.MachineName)
+	_, err = d.getClient().ActionStop(machine, &rancher.InstanceStop{})
+	return err
+}
+
+func (d *Driver) Remove() error {
+	machine, err := d.getMachine()
+	if err != nil {
+		return err
+	}
+
+	log.Debugf("Removing %s", d.MachineName)
+	return d.getClient().Delete(machine)
+}
+
+func (d *Driver) Restart() error {
+	machine, err := d.getMachine()
+	if err != nil {
+		return err
+	}
+
+	log.Debugf("Stopping %s", d.MachineName)
+	_, err = d.getClient().ActionRestart(machine)
+	return err
+}
+
+func (d *Driver) Kill() error {
+	return d.Stop()
+}
+
+func (d *Driver) getClient() rancher.VirtualMachineOperations {
+	if d.client == nil {
+		client, err := rancher.NewRancherClient(&rancher.ClientOpts{
+			Url:       d.Url,
+			AccessKey: d.AccessKey,
+			SecretKey: d.SecretKey,
+		})
+
+		if err != nil {
+			return nil
+		}
+
+		d.client = client
+	}
+
+	return d.client.VirtualMachine
+}
+
+func (d *Driver) publicSSHKeyPath() string {
+	return d.GetSSHKeyPath() + ".pub"
+}
